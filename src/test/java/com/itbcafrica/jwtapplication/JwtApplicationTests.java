5ed58336f20cc710133121b5ad25fe8cd7bb966e
bouendeu <raphael.bouendeu@itbcafrica.com>
2023-03-17 19:05:46 +0100

    added init code

---
+++ src/test/java/com/itbcafrica/jwtapplication/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itbcafrica.jwtapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
